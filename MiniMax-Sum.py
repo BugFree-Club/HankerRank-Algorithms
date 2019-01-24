5c34149e3833693950c9bc3a4723f94a2b3779b2
assassin945 <40627941+assassin945@users.noreply.github.com>
2019-01-24 22:36:51 +0800

    Create MiniMax-Sum.py

---
+++ MiniMax-Sum.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def miniMaxSum(arr):
+
+if __name__ == '__main__':
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
