2e5ba5aa1f52429b875e64f1b3f382754f7803f8
assassin945 <40627941+assassin945@users.noreply.github.com>
2019-01-25 00:17:18 +0800

    Add files via upload

---
+++ 2019-1-25/Plus-Minus.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def plusMinus(arr):
+
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
