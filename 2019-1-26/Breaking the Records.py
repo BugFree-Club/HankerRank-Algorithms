a83107c36e21dc42a0e2b2dcd6a79931143e4d1c
assassin945 <40627941+assassin945@users.noreply.github.com>
2019-01-25 23:42:26 +0800

    Create Breaking the Records.py

---
+++ 2019-1-26/Breaking the Records.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the breakingRecords function below.
+def breakingRecords(scores):
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    scores = list(map(int, input().rstrip().split()))
+
+    result = breakingRecords(scores)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
