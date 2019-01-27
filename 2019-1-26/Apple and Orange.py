a5a045cdc21cefc7ee12667fb339b7bfa3ac1e49
Tapiocac <44674588+Tapiocac@users.noreply.github.com>
2019-01-27 15:20:34 +0800

    1.26作业

--- 2019-1-26/Apple and Orange.py
+++ 2019-1-26/Apple and Orange.py
@@ -7,29 +7,45 @@ import re
 import sys
 
 # Complete the countApplesAndOranges function below.
-def countApplesAndOranges(s, t, a, b, apples, oranges):
+def countApplesAndOranges(s, t, a, b, apples, oranges, m, n):
+    apple=0
+
+    orange=0
+
+    for i in range(m) :
+        if a + apples[i] >= s and a + apples[i] <= t :
+            apple+=1
+            
+    for j in range(n) :
+        if b + oranges[j] >= s and b + oranges[j] <= t :
+            orange+=1
+            
+    print(apple)
+    
+    print(orange)
+
 
 if __name__ == '__main__':
     st = input().split()
 
-    s = int(st[0])
+    s = int(st[0])#房左
 
-    t = int(st[1])
+    t = int(st[1])#房右
 
     ab = input().split()
 
-    a = int(ab[0])
+    a = int(ab[0])#苹果树
 
-    b = int(ab[1])
+    b = int(ab[1])#橘子树
 
     mn = input().split()
 
-    m = int(mn[0])
+    m = int(mn[0])#苹果数
 
-    n = int(mn[1])
+    n = int(mn[1])#橘子数
 
     apples = list(map(int, input().rstrip().split()))
 
     oranges = list(map(int, input().rstrip().split()))
 
-    countApplesAndOranges(s, t, a, b, apples, oranges)
+    countApplesAndOranges(s, t, a, b, apples, oranges, m, n)
