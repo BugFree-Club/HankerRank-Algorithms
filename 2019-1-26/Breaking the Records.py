d2adf74ff2c67ca92c8d456f46342af3c3ac0e3b
Tapiocac <44674588+Tapiocac@users.noreply.github.com>
2019-01-27 15:23:38 +0800

    1.26作业

--- 2019-1-26/Breaking the Records.py
+++ 2019-1-26/Breaking the Records.py
@@ -8,6 +8,29 @@ import sys
 
 # Complete the breakingRecords function below.
 def breakingRecords(scores):
+    maxn=scores[0]
+    
+    minn=scores[0]
+    
+    H=0
+    
+    L=0
+    
+    for i in range(n) :
+        
+        if maxn<scores[i] :
+            
+            maxn=scores[i]
+            
+            H+=1
+            
+        if minn>scores[i] :
+            
+            minn=scores[i]
+            
+            L+=1
+            
+    return H,L
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
