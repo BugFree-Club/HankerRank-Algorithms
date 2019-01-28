599f2e9bd4db31c2f97bc2f6344a6d833b240909
Tapiocac <44674588+Tapiocac@users.noreply.github.com>
2019-01-28 20:56:07 +0800

    1.28作业

--- 2019-1-28/Migratory.py
+++ 2019-1-28/Migratory.py
@@ -8,6 +8,27 @@ import sys
 
 # Complete the migratoryBirds function below.
 def migratoryBirds(arr):
+        k=[0,0,0,0,0,0]
+
+    maxn=0
+
+    for i in range(arr_count) :
+
+        for j in range(1,6) :
+
+            if arr[i]==j :
+
+                k[j]+=1
+
+    for i in range(6) :
+
+        if k[i]>maxn :
+
+            maxn=k[i]
+
+            answer=i
+
+    return answer
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
