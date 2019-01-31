d49e176b69aaf70d6693ae65dc97701cf0ea0b81
assassin945 <40627941+assassin945@users.noreply.github.com>
2019-01-31 17:37:33 +0800

    Update Birthday-Cake-Candles.py

--- 2019-1-25/Birthday-Cake-Candles.py
+++ 2019-1-25/Birthday-Cake-Candles.py
@@ -7,6 +7,15 @@ import re
 import sys
 
 def birthdayCakeCandles(ar):
+    m = ar[0]
+    count = 0
+    for i in range(1, len(ar)):
+        if m < ar[i]:
+            m = ar[i]
+    for j in range(0, len(ar)):
+        if ar[j] == m:
+            count += 1
+    return count
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
